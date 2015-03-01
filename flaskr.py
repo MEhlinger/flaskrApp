7a9b6635835a30ac21ddfa8ce314f4bc7f3b50dd
Marshall Ehlinger <mehlinger333@gmail.com>
2015-03-01 21:24:18 +0000

    Added sqlite database requests

--- flaskr.py
+++ flaskr.py
@@ -1,10 +1,11 @@
-# flaskr.py from flask micorblog tutorial (flask.pocoo.org)
+# flaskr.py from flask microblog tutorial (flask.pocoo.org)
 # by Marshall Ehlinger
 
 # Imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
+from context import closing
 
 # Configuration
 DATABASE = '/tmp/flaskr.db'
@@ -20,6 +21,22 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 	
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resources('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit())	
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+	
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
 if __name__ == '__main__':
 	app.run()
 	
